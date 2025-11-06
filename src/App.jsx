fc88c381aaa97663862378a9b0fc111a721bf8aa
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-11-06 13:08:32 +0530

    resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Components/Login";
-import SignUp from "./Components/signup";
-import ProfilePage from "./Components/Profile";
-
-function App() {
-  const [count, setCount] = useState(0); // optional state
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/profile" element={<ProfilePage />} />
-=======
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './Components/HomePage'
 import Login from './Components/Login'
+import SignUp from "./Components/Signup";
+import ProfilePage from "./Components/Profile";
 
 function App() {
   return (
@@ -28,7 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
->>>>>>> a43951565dd362379d8fd5846cccb25a85cd06eb
+         <Route path="/Signup" element={<SignUp />} />
+        <Route path="/ProfilePage" element={ProfilePage} />
       </Routes>
     </BrowserRouter>
   )
